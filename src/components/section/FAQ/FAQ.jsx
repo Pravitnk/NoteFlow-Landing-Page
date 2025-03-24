9fd95da40d5a435c30cc48ecde6e3d9afdbdee33
PRAVIT <pravitnaik656@gmail.com>
2025-03-24 16:47:04 +0530

    `feat: Update FAQ component and FAQ list component with animations and state management`

--- src/components/section/FAQ/FAQ.jsx
+++ src/components/section/FAQ/FAQ.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import { motion } from "motion/react";
+import CaretUp from "../../icons/CaretUp";
 
-const FAQ = () => {
-  return <div>FAQ</div>;
+const FAQ = ({ questions, activeQuestions, handleQuestionClick }) => {
+  const itemVarients = {
+    hidden: {
+      opacity: 0,
+      y: 20,
+    },
+    visible: { opacity: 100, y: 0 },
+  };
+
+  return (
+    <motion.li variants={itemVarients} className="shrink-0 grow-0">
+      <button
+        className="flex w-full cursor-pointer items-center"
+        onClick={() => handleQuestionClick(questions.id)}
+      >
+        <div className="border-primary-50 mr-6 rounded-xl border-2 p-3.5">
+          <questions.Icon
+            width={2}
+            className="stroke-primary-50"
+            alt={questions.alt}
+          />
+        </div>
+
+        <p className="text-primary-50 mr-auto pr-4 text-left text-xl/loose font-semibold tracking-normal">
+          {questions.question}
+        </p>
+
+        <div className="flex h-12 w-12 shrink-0 items-center justify-center">
+          <CaretUp
+            className="stroke-primary-50"
+            activeQuestion={activeQuestions === questions.id}
+            width={2.5}
+            alt="Caret Up Icon"
+          />
+        </div>
+      </button>
+
+      <motion.p
+        className="text-primary-50 pt-0 pr-14 pl-20 text-lg/8 font-light"
+        initial={{ opacity: 0, maxHeight: 0, visibility: "hidden" }}
+        animate={
+          activeQuestions === questions.id
+            ? {
+                opacity: 1,
+                maxHeight: "300px",
+                visibility: "visible",
+                paddingTop: "1rem",
+              }
+            : {}
+        }
+        transition={{
+          duration: 0.3,
+          ease: "easeIn",
+        }}
+        layout
+      >
+        {questions.answer}
+      </motion.p>
+    </motion.li>
+  );
 };
 
 export default FAQ;
--- src/components/section/FAQ/FAQlist.jsx
+++ src/components/section/FAQ/FAQlist.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { React, useState } from "react";
+import { motion } from "motion/react";
+import FAQ from "./FAQ";
 
-const FAQlist = () => {
-  return <div>FAQlist</div>;
+const FAQlist = ({
+  category,
+  questions,
+  activeQuestions,
+  handleQuestionClick,
+}) => {
+  const [inView, setInView] = useState(false);
+  return (
+    <motion.ul
+      className="m-auto flex max-w-[826px] flex-col gap-y-14"
+      initial="hidden"
+      animate={inView ? "visible" : "hidden"}
+      key={category}
+      layout
+      variants={{
+        hidden: { opacity: 0 },
+        visible: {
+          transition: {
+            staggerChildren: 0.25,
+            ease: "easeIn",
+          },
+        },
+      }}
+      whileInView={{ opacity: 1 }}
+      onViewportEnter={() => setInView(true)}
+      onViewportLeave={() => setInView(false)}
+      viewport={{ once: false, amount: "100%" }}
+    >
+      {questions?.map((ques) => (
+        <FAQ
+          key={ques.id}
+          questions={ques}
+          activeQuestions={activeQuestions}
+          handleQuestionClick={handleQuestionClick}
+        />
+      ))}
+    </motion.ul>
+  );
 };
 
 export default FAQlist;
