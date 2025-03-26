ad3b9d976b8a5eeae029b20fe810c6609432675a
PRAVIT <pravitnaik656@gmail.com>
2025-03-26 13:09:01 +0530

    `Added useModalContent hook and updated Hero component to open sign-up modal on button click`

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ArrowRightLine from "../icons/ArrowRightLine";
 import ArrowRight from "../icons/ArrowRight";
 import HeroGraphic from "../../assets/graphics/HeroGraphic.webp";
+import { useModalContent } from "../../context/ModalContext";
 
 const Hero = () => {
+  const { setActiveModal } = useModalContent();
   return (
     <div className="m-auto grid max-w-[90rem] grid-cols-[5fr_4fr] items-center gap-x-18 px-24 py-42">
       {/* left  */}
@@ -15,7 +17,10 @@ const Hero = () => {
           Let AI organize & summarize your notes, <br />
           saving your time and boosting productivity
         </p>
-        <button className="text-primary-1300 bg-primary-500 border-primary-500 hover:border-primary-50 hover:bg-primary-50 transition-property primary-glow primary-glow-hover group flex cursor-pointer items-center justify-between gap-x-4 rounded-full border-2 px-8 py-3.5">
+        <button
+          className="text-primary-1300 bg-primary-500 border-primary-500 hover:border-primary-50 hover:bg-primary-50 transition-property primary-glow primary-glow-hover group flex cursor-pointer items-center justify-between gap-x-4 rounded-full border-2 px-8 py-3.5"
+          onClick={() => setActiveModal("sign-up")}
+        >
           <p className="text-lg/8">Get Started</p>
           <div className="w-5">
             <ArrowRightLine
